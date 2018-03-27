ac4fee02a3288afb777e5acb82d67d2d3b1755cb
Chayeen <chayeenhuang@126.com>
2018-03-27 13:39:46 +0800

    fix several format bugs, design that how to add the sound change rules into code.

--- krdivide.py
+++ krdivide.py
@@ -64,8 +64,11 @@ for each_file in data_files:
                 else :  
                     # continue
                     temp_line = temp_line[:-1]  + each_line[i]
+                    # temp_line = temp_line  + each_line[i]
+            # temp_line = temp_line[:-1]
             temp_line = each_line.strip('\n') +  '\n'  +   temp_line    
             temp_file_string = temp_file_string + temp_line +  '\n' 
+        temp_file_string = temp_file_string[:-2]
         print ( each_file + "-"*5 + ">finished")   #finish  
         #the name of new file to save the result, the new file is in the current dir  
         new_filename = "./divide/"+os.path.splitext(os.path.basename(each_file))[0] + ".txt"  
